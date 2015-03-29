73fcee31eec06edac9ed0f71e1698669f87174c9
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2015-03-29 21:15:10 +0200

    Make a simple implementation of a hash-based plain and compressed trie.

---
+++ hash_trie/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+'''Hash-based compressed trie - represents a set of strings.'''
+
+from .hash_trie import *
+
+__version__ = '0.1'
+__author__ = 'Bohumír Zámečník <bohumir.zamecnik@.gmail.com>'
